f8635060ee451db8b9a47d689438f25e7792f6c9
Suresh Reddy <sureshreddy.per@gmail.com>
2025-01-29 03:12:16 +0530

    Update transaction-history.entity.ts

--- src/transactions/entities/transaction-history.entity.ts
+++ src/transactions/entities/transaction-history.entity.ts
@@ -25,6 +25,7 @@ export class TransactionHistory extends BaseEntity {
   event: TransactionEvent;
 
   @Column({
+    name: "old_status",
     type: "enum",
     enum: TransactionStatus,
     nullable: true,
@@ -32,6 +33,7 @@ export class TransactionHistory extends BaseEntity {
   oldStatus?: TransactionStatus;
 
   @Column({
+    name: "new_status",
     type: "enum",
     enum: TransactionStatus,
     nullable: true,
